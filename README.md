431764944896a26febbb5fa63dd60dfb9e22088c
Quinny <siramnot@gmail.com>
2017-06-25 00:37:03 +0300

    Initial commit!
    Let's get this party started!

---
+++ README.md
@@ -0,0 +1,4 @@
+# discord-client
+## WIP!
+A better client...
+**More info, soon!**
---
+++ src/main/java/com/github/quinnthusiast/discord_client/App.java
@@ -0,0 +1,9 @@
+package com.github.quinnthusiast.discord_client;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        // hi initial commit
+    }
+}
